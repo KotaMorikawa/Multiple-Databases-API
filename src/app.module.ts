af763d285ff4ee9dcb9e3b8bd497dae78bcdf47f
KotaMorikawa <koke.kokota0130@gmail.com>
2023-09-04 18:41:33 +0900

    ベース作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { MBookModule } from './m_book/m_book.module';
+import { MCatalogModule } from './m_catalog/m_catalog.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, MBookModule, MCatalogModule],
   controllers: [AppController],
   providers: [AppService],
 })
