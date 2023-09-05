f5a7cd4c7792870219effd9f294d2776f9a43e97
KotaMorikawa <koke.kokota0130@gmail.com>
2023-09-05 17:17:42 +0900

    ローカルLambda動作確認

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,42 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Server } from 'http';
+import express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { createServer, proxy } from '@vendia/serverless-express';
+import { eventContext } from '@vendia/serverless-express/middleware';
+import { Handler, Context } from 'aws-lambda';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+const binaryMimeTypes: string[] = [];
+
+let cachedServer: Server;
+let count = 0;
+
+const bootstrapServer = async (module: any): Promise<Server> => {
+  if (!cachedServer) {
+    console.log('Bypassed server cache');
+    const expressApp = express();
+    const nestApp = await NestFactory.create(
+      module,
+      new ExpressAdapter(expressApp),
+    );
+    nestApp.enableCors();
+    nestApp.use(eventContext());
+    await nestApp.init();
+    cachedServer = createServer(expressApp, undefined, binaryMimeTypes);
+  }
+
+  count += 1;
+  console.log(
+    `[${module?.name || 'Unknown module'}]`,
+    'Server bootstrapped',
+    count,
+    'times.',
+  );
+  return cachedServer;
+};
+
+export const handler: Handler = async (event: any, context: Context) => {
+  const server = await bootstrapServer(AppModule);
+  return proxy(server, event, context, 'PROMISE').promise;
+};
